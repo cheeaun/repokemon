e598fc4f3ff07fe5cc17fd0baaf420d1f8ef3523
Lim Chee Aun <cheeaun@gmail.com>
2017-06-19 22:06:36 +0800

    Gemoji-fy the repo descriptions
    Convert :emojicode: to actual emoji unicode characters

---
+++ scripts/gemoji.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const gemoji = require('gemoji');
+
+let index = 1;
+const data = JSON.parse(fs.readFileSync('data/repokemon.json'));
+data.forEach(({repo}) => {
+  if (repo && repo.desc && /:[^\s:]+:/.test(repo.desc)){
+    const exp = /([^:]*)(:[^\s:]+:)([^:]*)/g;
+    const desc = repo.desc.replace(exp, (match, text1, emojicode, text2) => {
+      const emoji = gemoji.name[emojicode.slice(1, -1)].emoji;
+      return text1 + emoji + text2;
+    });
+    console.log(index++ + '. [' + repo.full_name + '] ' + desc);
+    repo.desc = desc; // Mutate back
+  }
+});
+
+fs.writeFileSync('data/repokemon.json', JSON.stringify(data, null, '\t'));
