3101d46f95a3e441ece8c3f4f0763ab1c3565114
n4n5 <its.just.n4n5@gmail.com>
2025-01-17 14:51:34 +0100

    update

--- scripts/gemoji.mjs
+++ scripts/gemoji.mjs
@@ -1,18 +1,23 @@
-import fs from 'fs';
-import { nameToEmoji } from 'gemoji';
+import fs from "fs";
+import { nameToEmoji } from "gemoji";
 
-let index = 1;
-const data = JSON.parse(fs.readFileSync('data/repokemon.json'));
-data.forEach(({ repo }) => {
-  if (repo && repo.desc && /:[^\s:]+:/.test(repo.desc)) {
-    const exp = /([^:]*)(:[^\s:]+:)([^:]*)/g;
-    const desc = repo.desc.replace(exp, (match, text1, emojicode, text2) => {
-      const emoji = nameToEmoji[emojicode.slice(1, -1)];
-      return text1 + emoji + text2;
+const langs = fs.readdirSync("data/langs");
+for (const oneLang of langs) {
+    console.log(`Processing ${oneLang}...`);
+
+    let index = 1;
+    const data = JSON.parse(fs.readFileSync(`data/langs/${oneLang}/repokemon.json`));
+    data.forEach(({ repo }) => {
+        if (repo && repo.desc && /:[^\s:]+:/.test(repo.desc)) {
+            const exp = /([^:]*)(:[^\s:]+:)([^:]*)/g;
+            const desc = repo.desc.replace(exp, (match, text1, emojicode, text2) => {
+                const emoji = nameToEmoji[emojicode.slice(1, -1)];
+                return text1 + emoji + text2;
+            });
+            console.log(index++ + ". [" + repo.full_name + "] " + desc);
+            repo.desc = desc; // Mutate back
+        }
     });
-    console.log(index++ + '. [' + repo.full_name + '] ' + desc);
-    repo.desc = desc; // Mutate back
-  }
-});
 
-fs.writeFileSync('data/repokemon.json', JSON.stringify(data, null, '\t'));
+    fs.writeFileSync(`data/langs/${oneLang}/repokemon.json`, JSON.stringify(data, null, "\t"));
+}
